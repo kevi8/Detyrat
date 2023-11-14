590874934fc54339a9104cbef0bc567fc067bfc0
kevi8 <likollarikevi@gmail.com>
2023-11-14 20:46:21 +0100

    First Commit

--- testTry/Program.cs
+++ testTry/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<string> cars = new List<string>();
+
+cars.Add("volkswagen");
+cars.Add("Mercedes");
+cars.Add("BMW");
+cars.Add("Audi");
+cars.Add("Peguot");
+
+int markat = cars.Count();
+
+System.Console.WriteLine(markat);
+System.Console.WriteLine(cars[2]);
+
+cars.RemoveAt(2);
+int markat2 =cars.Count();
+System.Console.WriteLine(markat2);
