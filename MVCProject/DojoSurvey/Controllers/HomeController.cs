4afa2e54af998432b6e8040032418d9dbbe21930
kevi8 <likollarikevi@gmail.com>
2023-12-02 00:17:56 +0100

    First Commit

---
+++ MVCProject/DojoSurvey/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers;
+
+public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpGet("survey")]
+        public IActionResult ReturnedData()
+        {
+            return View();
+        }
+
+        [HttpPost("survey")]
+        public IActionResult Returned()
+        {
+            return View();
+        }
+    }
